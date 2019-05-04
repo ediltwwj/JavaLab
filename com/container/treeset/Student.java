e5073e33e0f9b67af208b3c0ea96b173b9a1b85d
13967 <1396748139@qq.com>
2019-05-04 16:59:38 +0800

    学生类，并重写相关方法

--- com/container/treeset/Student.java
+++ com/container/treeset/Student.java
@@ -1,6 +1,8 @@
 package com.container.treeset;
 
-public class Student {
+import java.util.*;
+
+public class Student implements Comparable {
     private String stuId;  // 学号
     private String stuName;  // 姓名
     private int totalScore;  // 总分
@@ -29,8 +31,40 @@ public class Student {
         this.totalScore = totalScore;
     }
 
+    public Student(String stuId, String stuName, int totalScore) {
+        this.stuId = stuId;
+        this.stuName = stuName;
+        this.totalScore = totalScore;
+    }
+
     @Override
     public String toString() {
         return "学号: " + this.stuId + ", 姓名: " + this.stuName + ", 总分: " + this.totalScore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student))
+            return false;
+
+        Student s = (Student) o;
+
+        if (this.stuId.equals(s.stuId))
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Student s = (Student) o;
+
+        int result = this.totalScore > s.totalScore ? 1 : (this.totalScore == s.totalScore ? 0 : -1);
+        result = -result;  // 因为是降序
+
+        if (0 == result)
+            result = this.stuName.compareTo(s.stuName); // compareTo比较对象
+
+        return result;
     }
 }
