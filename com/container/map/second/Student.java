d2d2758fbd6957a77bbd692536a746f29462a541
13967 <1396748139@qq.com>
2019-05-04 17:25:33 +0800

    Map之第二题学生类

---
+++ com/container/map/second/Student.java
@@ -0,0 +1,41 @@
+package com.container.map.second;
+
+public class Student {
+    private String name;  // 姓名
+    private int age;  // 年龄
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + ", Age: " + this.age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof com.container.map.second.Student))
+            return false;
+
+        Student s = (Student) o;
+
+        if (this.name.equals(s.name) && this.age == s.age)
+            return true;
+        else
+            return false;
+    }
+}
+
